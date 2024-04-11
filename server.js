c72ec3747dbcdf970718dc5c2f5acad128606e09
mursal-r <mursalrahimi.ga@gmail.com>
2024-04-11 09:33:07 -0700

    update folder/files setup

--- server.js
+++ server.js
@@ -3,6 +3,12 @@ const path = require('path');
 const favicon = require('serve-favicon');
 const logger = require('morgan');
 
+
+require('dotenv').config()
+
+// connect to mongo db
+require('./config/database');
+
 const app = express();
 
 app.use(logger('dev'));
