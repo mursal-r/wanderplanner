80ecc3bbdd0a8019b5dc029550a60960b58c8bcc
mursal-r <mursalrahimi.ga@gmail.com>
2024-04-13 12:35:41 -0700

    HomePage update

---
+++ src/components/PopularDestinations/PopularDestinations.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function PopularDestinations() {
+    return (
+        <div>
+            <h2>Popular Destinations</h2>
+            {/* Add my list of popular destions here */}
+        </div>
+    );
+}
+
+export default PopularDestinations;
+
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,7 +1,20 @@
+import React from 'react';
+import './HomePage.css'; // Import CSS file for styling
+
 export default function HomePage() {
     return (
-        <>
-            <h1> Home Page</h1>
-        </>
-    )
+        <div className="container">
+            <div className="header">
+                <div className="header-content">
+                    <img src="/images/mainImage.png" alt="Fun activities" />
+                    <h1>Explore fun activities for your next destination with ease</h1>
+                </div>
+            </div>
+            <div className="search-container">
+                <input type="text" placeholder="Destination lookup" />
+                <button className="search-button">Search</button>
+            </div>
+
+        </div>
+    );
 }
