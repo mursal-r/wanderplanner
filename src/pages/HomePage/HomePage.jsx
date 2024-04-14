42e15253c4040e4bb3e8248974702ce073af7109
mursal-r <mursalrahimi.ga@gmail.com>
2024-04-13 17:58:31 -0700

    add TouristAttractionCard

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -8,15 +8,14 @@ export default function HomePage() {
             <div className="header">
                 <div className="header-content">
                     <img src="/images/mainImage.png" alt="Fun activities" />
-                    <h1>Explore fun activities for your next destination with ease</h1>
+                    <h1>Explore fun activities for your next destination with ease.</h1>
                 </div>
             </div>
             <div className="search-container">
-                <input type="text" placeholder="Destination lookup" />
+                <input type="text" placeholder="destination lookup📍" />
                 <button className="search-button">Search</button>
             </div>
-            <div><PopularDestinations/></div>
-
+            <PopularDestinations />
         </div>
     );
 }
