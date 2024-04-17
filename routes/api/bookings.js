486a0d02f7b5d74e66e918a28a6a877bcc9b3872
mursal-r <mursalrahimi.ga@gmail.com>
2024-04-17 01:30:23 -0700

    test booking routing

--- routes/api/bookings.js
+++ routes/api/bookings.js
@@ -1,8 +1,14 @@
 const express = require('express');
-
 const router = express.Router();
 const bookingCtrl = require('../../controllers/api/bookings');
 
+// POST route for creating a new booking
 router.post('/', bookingCtrl.create);
 
+// GET route for fetching all bookings
+router.get('/', bookingCtrl.getAll);
+
 module.exports = router;
+
+
+//ADDED A NEW ROUTE TO HANDLE GET REQUESTS FOR FETCHING ALL BOOKINGS AND USE getBookings function from controller
